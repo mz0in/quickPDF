e040b96d20cda1189fdd3326df6a78d8a2ac8cbd
aianshume <anshumeena1947@gmail.com>
2023-04-28 10:37:40 +0530

    wokring on components

--- src/renderer/src/components/Editor/plugins/componentsPlugin/index.ts
+++ src/renderer/src/components/Editor/plugins/componentsPlugin/index.ts
@@ -3,32 +3,10 @@
  */
 
 import { Editor } from 'grapesjs'
+import { templates } from './templates'
 
-export default (editor: Editor, opts = {}) => {
-  const defaults = {}
-
-  const options = { ...defaults, ...opts }
-
-  const customBlock1 = {
-    id: 'my-custom-block-1',
-    label: 'My Custom Block 1',
-    content: '<div class="my-custom-block-1">Hello, world!</div>',
-    category: 'Custom Blocks',
-    attributes: {
-      class: 'my-custom-block-1'
-    }
-  }
-
-  const customBlock2 = {
-    id: 'my-custom-block-2',
-    label: 'My Custom Block 2',
-    content: '<div class="my-custom-block-2">Goodbye, world!</div>',
-    category: 'Custom Blocks',
-    attributes: {
-      class: 'my-custom-block-2'
-    }
-  }
-
-  editor.BlockManager.add(customBlock1.id, customBlock1)
-  editor.BlockManager.add(customBlock2.id, customBlock2)
+export default (editor: Editor) => {
+  templates.map(template => {
+    editor.BlockManager.add(template.id, template)
+  })
 }
