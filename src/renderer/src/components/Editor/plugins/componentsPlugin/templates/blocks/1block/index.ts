5686b86e67f31ce92e212ffbb016f44f5af39283
Ai Anshu <anshumeena1947@gmail.com>
2023-06-26 14:34:27 +0530

    add: components

--- src/renderer/src/components/Editor/plugins/componentsPlugin/templates/blocks/1block/index.ts
+++ src/renderer/src/components/Editor/plugins/componentsPlugin/templates/blocks/1block/index.ts
@@ -55,7 +55,35 @@ const blocks = [
     content: `<div id="ih8gcn"><div id="ir0n1j"><span id="ice0uc">उधर गृहमंत्री अमित शाह ने विपक्षी एकता पर तंज कसा</span></div><div id="iqw5pc"><span id="ij75tl">को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज कसे</span></div><div id="ie7r2z"><div id="i79ul9"><img id="i7t8do" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3R5bGU9ImZpbGw6IHJnYmEoMCwwLDAsMC4xNSk7IHRyYW5zZm9ybTogc2NhbGUoMC43NSkiPgogICAgICAgIDxwYXRoIGQ9Ik04LjUgMTMuNWwyLjUgMyAzLjUtNC41IDQuNSA2SDVtMTYgMVY1YTIgMiAwIDAgMC0yLTJINWMtMS4xIDAtMiAuOS0yIDJ2MTRjMCAxLjEuOSAyIDIgMmgxNGMxLjEgMCAyLS45IDItMnoiPjwvcGF0aD4KICAgICAgPC9zdmc+"/></div><div id="iqqcux"><div id="irwlfl"><span id="ioj95d">को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज</span></div></div></div><div id="i9s6vg"><div id="ihmozh"><div id="igrx6j"><span id="iuwvm2">को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज कसे, उनका कहना है कि विपक्षी एकता सिर्फ़ दिखावा है। ये पार्टियाँ कभी एक नहीं हो सकतीं क्योंकि अलग- अलग राज्यों में इनके अपने- अपने हित हैं </span><span id="icqh17">को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज कसे, उनका कहना है कि विपक्षी एकता सिर्फ़ दिखावा है।</span></div></div></div></div>
     <style>
  #ih8gcn{min-height:75px;flex-grow:1;flex-basis:30%;}#i9s6vg{display:flex;justify-content:flex-start;align-items:stretch;flex-wrap:nowrap;padding:0px 0px 0px 0px;}#ihmozh{min-height:75px;flex-grow:1;flex-basis:calc(100% / 1);}#ir0n1j{padding:10px;text-align:center;font-size:large;}#ice0uc{font-weight:600;text-align:center;}#iqw5pc{padding:5px 5px 5px 5px;}#ij75tl{font-size:small;}#ie7r2z{display:flex;justify-content:flex-start;align-items:stretch;flex-wrap:nowrap;padding:0px 5px 0px 5px;}#i79ul9{min-height:75px;flex-grow:1;flex-basis:calc(100% / 2);}#iqqcux{min-height:75px;flex-grow:1;flex-basis:calc(100% / 2);}#i7t8do{color:black;height:114px;}#irwlfl{padding:5px 2px 5px 2px;}#ioj95d{font-size:small;}#igrx6j{padding:10px;}#iuwvm2{font-size:small;}#icqh17{font-size:small;}`
-  }
+  },
+  {
+    media : `<img height="120px" width=${width} class="firstBlockImages" src=${images[7]} />`,
+    category: category,
+    content: `<div id="iultwk"><div id="i3vvhh">भगवान जगन्नाथ, बहन सुभद्रा और बड़े भाई बलदाऊ</div><div id="ictjiz"><b draggable="true" id="ilxys2">इस मीटिंग |</b><span id="inp1l8"> को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज कसे, उनका कहना है कि विपक्षी एकता सिर्फ़ दिखावा है। ये पार्टियाँ कभी एक नहीं हो सकतीं क्योंकि अलग- अलग राज्यों में इनके अपने- अपने हित हैं जिनसे ये कभी समझौता नहीं कर सकतीं। अब्दुल्ला ने तो इस मीटिंग में ही आप पार्टी पर तंज कस दिया कि धारा 370 पर आप के रुख़ को लेकर मैं आश्चर्यचकित हूँ। इस मीटिंग को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज कसे, उनका कहना है कि विपक्षी एकता सिर्फ़ दिखावा है। ये पार्टियाँ कभी एक नहीं हो सकतीं क्योंकि अलग- अलग राज्यों में इनके अपने- अपने हित हैं </span></div></div>
+    <style>
+ #iultwk{min-height:75px;flex-grow:1;flex-basis:calc(100% / 7);}#i3vvhh{padding:10px 10px 5px 10px;text-align:center;font-weight:600;}#ilxys2{font-size:small;}#inp1l8{font-size:small;text-align:left;}#ictjiz{padding:5px 10px 5px 10px;text-align:left;}`
+  },
+  {
+    media : `<img height="80px" width="50px" class="firstBlockImages" src=${images[10]} />`,
+    category: category,
+    content: `<div id="i14d17"><img id="im67qg" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3R5bGU9ImZpbGw6IHJnYmEoMCwwLDAsMC4xNSk7IHRyYW5zZm9ybTogc2NhbGUoMC43NSkiPgogICAgICAgIDxwYXRoIGQ9Ik04LjUgMTMuNWwyLjUgMyAzLjUtNC41IDQuNSA2SDVtMTYgMVY1YTIgMiAwIDAgMC0yLTJINWMtMS4xIDAtMiAuOS0yIDJ2MTRjMCAxLjEuOSAyIDIgMmgxNGMxLjEgMCAyLS45IDItMnoiPjwvcGF0aD4KICAgICAgPC9zdmc+"/><div id="i4i3rs">उस दिन पकवान बनते हैं, पड़ोसी, दोस्त और रिश्तेदारों को बुलाया जाता है। इस त्योहार को मनाने की वजह बेहद खूबसूरत है। माता-पिता इससे अपनी बेटी के ससुराल वालों को सौहार्द, प्रेम का संदेश देते हैं। दोनों परिवारों के रिश्ते गहरे हों, इसकी कामना करते हैं।उस दिन पकवान बनते हैं, पड़ोसी, दोस्त और रिश्तेदारों को बुलाया जाता है। इस त्योहार को मनाने की वजह बेहद खूबसूरत है। माता-पिता इससे अपनी बेटी के ससुराल वालों को सौहार्द, प्रेम का संदेश देते हैं। </div></div>
+    <style>
+#i4i3rs{padding:5px 5px 5px 5px;font-size:small;}#i14d17{min-height:75px;flex-grow:1;flex-basis:calc(100% / 2);}#im67qg{color:black;width:196px;height:106px;}`
+  },
+  {
+    media : `<img height="80px" width="50px" class="firstBlockImages" src=${images[9]} />`,
+    category: category,
+    content: `<div id="ivcqwg"><div id="i6zww6"><span id="i28zr8-3" draggable="true"><b id="ioz2ei">भगवान जगन्नाथ, बहन सुभद्रा और बड़े भाई बलदाऊ</b></span></div><img id="i598el" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3R5bGU9ImZpbGw6IHJnYmEoMCwwLDAsMC4xNSk7IHRyYW5zZm9ybTogc2NhbGUoMC43NSkiPgogICAgICAgIDxwYXRoIGQ9Ik04LjUgMTMuNWwyLjUgMyAzLjUtNC41IDQuNSA2SDVtMTYgMVY1YTIgMiAwIDAgMC0yLTJINWMtMS4xIDAtMiAuOS0yIDJ2MTRjMCAxLjEuOSAyIDIgMmgxNGMxLjEgMCAyLS45IDItMnoiPjwvcGF0aD4KICAgICAgPC9zdmc+"/><div id="ic46bj"><b draggable="true" id="i7gjiq">इस मीटिंग |</b><span id="ibz8rr"> को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज कसे, उनका कहना है कि विपक्षी एकता सिर्फ़ दिखावा है। ये पार्टियाँ कभी एक नहीं हो सकतीं क्योंकि अलग- अलग राज्यों में इनके अपने- अपने हित हैं जिनसे ये कभी समझौता नहीं कर सकतीं।</span></div></div>
+    <style>
+ #ivcqwg{min-height:75px;flex-grow:1;flex-basis:3%;}#i6zww6{padding:10px;}#i28zr8-3{font-size:large;}#i598el{color:black;width:235px;height:113px;}#i7gjiq{font-size:small;}#ibz8rr{font-size:small;text-align:left;}#ic46bj{padding:5px 0px 5px 0px;text-align:left;}`
+  },
+  {
+    media : `<img height="50px" width=${width} class="firstBlockImages" src=${images[8]} />`,
+    category: category,
+    content: `<div id="ilzzja"><div id="imxy8q"><b id="irgu9f">उधर गृहमंत्री अमित शाह ने विपक्षी एकता पर तंज कसा</b></div><div id="ikrwyu"><b draggable="true" id="ipudv5">इस मीटिंग |</b><span id="iqyxre"> को लेकर कई भाजपा नेताओं ने नीतीश कुमार और ख़ासकर, कांग्रेस के खिलाफ खूब तंज कसे, उनका कहना है कि विपक्षी एकता सिर्फ़ दिखावा है। ये पार्टियाँ कभी एक नहीं हो सकतीं क्योंकि अलग- अलग राज्यों में इनके अपने- अपने हित हैं जिनसे ये कभी समझौता नहीं कर सकतीं। अब्दुल्ला ने तो इस मीटिंग में ही </span></div></div>
+    <style>
+ #ipudv5{font-size:small;}#iqyxre{font-size:small;}#ikrwyu{padding:0px 10px 5px 10px;}#ilzzja{min-height:75px;flex-grow:1;flex-basis:calc(100% / 1);}#imxy8q{padding:10px;text-align:center;}`
+  },
   ]
 
 export default blocks;
